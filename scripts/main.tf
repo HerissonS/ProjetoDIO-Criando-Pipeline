190b06b718f96993fdcac03f8a38ee6c65d92f5c
Herisson Silva <antonioherisson@gmail.com>
2023-01-26 00:03:26 -0300

    Criado projeto

---
+++ scripts/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+
+   backend "gcs" {
+    bucket  = "herisson-desafio-dio"
+    prefix  = "terraform/state"
+  }
+  
+}
+
+provider "google" {
+  project = "agile-apex-369119"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags = ["prod"]
+
+  labels = {
+    centro_custo = "${var.centro_custo_rh}"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
---
+++ scripts/variables.tf
@@ -0,0 +1,13 @@
+variable "network_name" {
+  description = "Nome da Rede"
+  type        = string
+  default     = "default"
+}
+
+variable "centro_custo_rh" {
+  description = "Nome da Rede"
+  type        = string
+  default     = "rh"
+}
+
+
